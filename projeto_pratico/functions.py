fccc179fecc189155cbdc3d9709c0ab37036c0db
Darley Soares <darleysoares27@gmail.com>
2020-08-02 20:25:21 -0300

    atualização de visuais

--- projeto_pratico/functions.py
+++ projeto_pratico/functions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import main
 import streamlit as st
 import pandas as pd 
 import numpy as np 
--- projeto_pratico/main.py
+++ projeto_pratico/main.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import streamlit as st
-import pandas as pd 
-import numpy as np 
-import pystaticplot as ps
+
 import functions as fun
-import pipeline as pp
 
 if __name__ == '__main__':
     
